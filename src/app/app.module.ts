43603cae9d660b92735c2ee4b4d10549c9e59181
Edistiven <esu.matabay@yavirac.edu.ec>
2019-01-30 18:05:32 -0500

    Cliente Realizado en Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthGuard } from './shared';
+
+import { HttpModule} from '@angular/http';
+
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export const createTranslateLoader = (http: HttpClient) => {
+
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+};
+
+@NgModule({
+    imports: [
+        HttpModule,
+        CommonModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: createTranslateLoader,
+                deps: [HttpClient]
+            }
+        }),
+        AppRoutingModule
+    ],
+    declarations: [AppComponent],
+    providers: [AuthGuard],
+    bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/servicios/account.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+
+  constructor(private http: Http) { }
+
+
+
+
+}
